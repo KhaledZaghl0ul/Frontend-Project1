88a6d72f4d794088be3a8d57ecbe5e862bc76dae
Khaled Zaghloul <khaledzaghloul01@gmail.com>
2024-12-10 23:14:49 +0200

    task4

---
+++ Task 4/script.js
@@ -0,0 +1,57 @@
+/*
+- create function Return a Boolean if a number is divisible by 10
+
+ var num = +prompt("Enter num")
+var result
+var test = function (num) {
+    if( num % 10 === 0){
+        result = true
+        alert(result)
+
+        return 
+    }
+    else{
+    result = false
+    }
+    alert(result)
+
+    return
+
+
+}
+test(num)
+
+*/
+
+/*
+// - Create a function that finds the maximum number in an array
+
+var arr =[2,8,9,5,6,7,5,155]
+
+var test1 = function(arr) {
+    if ( arr.length === 0) {
+    alert("invalid arr")
+    }
+
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i]; 
+        }
+    }
+    alert(max)
+    return ;
+}
+test1(arr);
+*/
+/*
+- Create a function that reverses an array
+
+var arr =[2,8,9,5,6,7,5,155]
+var test3 = function(arr){
+alert(arr.reverse()  )
+return
+}
+test3();
+
+*/
